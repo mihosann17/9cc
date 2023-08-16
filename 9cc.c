f9e10b04bf64bf865bf1d746bd6444c9543f044b
jentluna <09b15087@gmail.com>
2023-08-16 12:51:31 +0900

    エラーメッセージを直観的に理解できるように改良

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+// 入力プログラム
+char *user_input;
+
+//エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr,"%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+
 //トークンの種類
 typedef enum{
   TK_RESERVED,  //記号
@@ -25,16 +43,6 @@ struct Token{
 //現在着目しているトークン
 Token *token;
 
-//エラーを報告するための関数
-//printfと同じ引数を取る
-void error(char *fmt, ...){
-  va_list ap;
-  va_start(ap,fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
 //次のトークンが期待している記号の時には、トークンを１つ読み進めて
 //真を返す。それ以外の場合には偽を返す。
 bool consume(char op) {
@@ -48,7 +56,7 @@ bool consume(char op) {
 //それ以外の場合にはエラーを報告する。
 void expect(char op){
   if(token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
@@ -56,7 +64,7 @@ void expect(char op){
 //それ以外の場合にはエラーを報告する。
 int expect_number(){
   if(token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -98,7 +106,7 @@ Token *tokenize(char *p){
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(token->str, "トークナイズできません");
   }
   
   new_token(TK_EOF, cur, p);
@@ -111,6 +119,8 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  //入力全体を保管する
+  user_input = argv[1];
   //トークナイズする
   token = tokenize(argv[1]);
 
